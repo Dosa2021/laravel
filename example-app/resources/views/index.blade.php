6b7916bf0caee67093c8fd7ea418969c2cb2c8b7
dosaken <dosa@dosakennoMacBook-Pro.local>
2026-01-04 09:36:55 +0900

    indexファイル（blade）作成

---
+++ example-app/resources/views/index.blade.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="ja">
+<head>
+  <meta charset="UTF-8">
+  <title>サンプルHTMLページ</title>
+  <link rel="stylesheet" href="{{ url('style.css') }}">
+</head>
+<body>
+  <div class='container'>
+  <h1>Posts</h1>
+    <ul>
+      <li>post</li>
+      <li>post</li>
+      <li>post</li>
+    </ul>
+  </div>
+</body>
+</html>
